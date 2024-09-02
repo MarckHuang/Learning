73550f5e6eb6fa5e3d42a3f09c9829b502195c8f
MarckHuang <76237925+MarckHuang@users.noreply.github.com>
2024-09-02 22:34:29 +0800

    Update solution.py
    Modify error code

--- Leetcode/Stack & Queue/20. Valid Parentheses/solution.py
+++ Leetcode/Stack & Queue/20. Valid Parentheses/solution.py
@@ -36,8 +36,8 @@ class Solution:
             '{':'}'
         }
         for item in s:
-            if item in mapping.key():
-                stack.appned(mapping[item])
+            if item in mapping.keys():
+                stack.append(mapping[item])
             elif not stack or stack[-1] != item:
                 return False 
             else:
