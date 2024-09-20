7a467b3c4d32fa28a0fc928fe10f0decdf1b55d9
MarckHuang <76237925+MarckHuang@users.noreply.github.com>
2024-09-20 21:00:35 +0800

    Add files via upload
    Upload solution code

---
+++ Leetcode/Tree/104. Maximum Depth of Binary Tree/solution.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#solution1
+#BFS
+from collections import deque
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([root])
+        depth = 0
+        while queue:
+            depth +=1
+            level_size = len(queue)
+            for i in range(level_size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
+    
+#solution2
+#DFS
+#下至上
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height,right_height)+1
+
+#solution3
+#DFS
+#上至下
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(node, depth):
+            if node is None:
+                return
+            depth +=1
+            nonlocal ans
+            ans = max(ans, depth)
+            dfs(node.left, depth)
+            dfs(node.right, depth)
+        dfs(root, 0)
+        return ans
